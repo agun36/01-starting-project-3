3664bd96c9e243bffc24237e1739118ea99cdd91
agun36 <aguntoroti10@gmail.com>
2024-02-10 11:10:07 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom/client';
+import * as Sentry from "@sentry/react";
+
+import App from './App.jsx';
+import './index.css';
+
+Sentry.init({
+  dsn: "https://b413798cd53a9b96ccc81342f93b86d8@o4506721842102272.ingest.sentry.io/4506721844396032",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
